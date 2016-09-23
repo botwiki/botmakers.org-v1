9205879cf5a05375e94b62cf00008c94c3ba0ebd
Stefan Bohacek <stefan@fourtonfish.com>
2016-09-23 10:49:56 -0400

    CSS fix for Firefox, minor cleanup.

--- server.js
+++ server.js
@@ -82,5 +82,5 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3011);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 server.listen(app.get('port') ,app.get('ip'), function(){
-  console.log('Express server listening on port' + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
